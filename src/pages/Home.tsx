294e7de57351f1147c160de518a5f2b0f2335c9f
GiovanniCrabi <crabigiovanni@gmail.com>
2021-07-08 23:04:40 -0300

    Modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,32 @@
+import { useContext } from 'react';
 import { useHistory } from "react-router-dom";
+
+import { auth, firebase } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from '../components/Button';
+
+import { TestContext } from '../App';
+
 import '../styles/auth.scss';
+
 
 
 export function Home(){
   const history = useHistory();
+  const value = useContext(TestContext);
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+
+      history.push('/rooms/new');
+    })
   }
 
   return (
@@ -23,9 +37,10 @@ export function Home(){
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
+        <h1> {value} </h1>
         <div className="main-content">
-          <img src={ logoImg } alt="Letmeask" />
-          <button onClick= { navigateToNewRoom } className="button-home">
+          <img src={ logoImg } alt="Letmeak" />
+          <button onClick= { handleCreateRoom } className="button-home">
           <img src={ googleIconImg } alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
